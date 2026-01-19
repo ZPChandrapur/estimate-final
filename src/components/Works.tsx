43739b9cd6a6bdda702b08b570d86d2db8afa6bf
ZPChandrapur <dev.zpchandrapur@gmail.com>
2026-01-19 12:18:57 +0530

    Updated Works.tsx

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -973,8 +973,9 @@ const Works: React.FC = () => {
                     onChange={(e) => setNewWork({ ...newWork, type: e.target.value })}
                     className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                   >
+                    <option value="Technical Approval">Technical Approval (TA)</option>
                     <option value="Technical Sanction">Technical Sanction (TS)</option>
-                    <option value="Administrative Approval">Administrative Approval (AA)</option>
+                   
                   </select>
                 </div>
 
